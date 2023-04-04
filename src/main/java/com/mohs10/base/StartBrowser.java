80d71f4f1c816196c95730584dc4039609deec86
dipanwita1159 <112863643+dipanwita1159@users.noreply.github.com>
2023-04-04 14:59:44 +0530

    Update StartBrowser.java

--- src/main/java/com/mohs10/base/StartBrowser.java
+++ src/main/java/com/mohs10/base/StartBrowser.java
@@ -45,9 +45,9 @@ public class StartBrowser {
 		  WebDriverManager.chromedriver().setup();
 		  ChromeOptions options = new ChromeOptions();
 		    options.setBinary("C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe");
-		  //WebDriverManager.firefoxdriver().setup();
+		//  WebDriverManager.firefoxdriver().setup();
 		 // WebDriverManager.edgedriver().setup();
-		  driver = new ChromeDriver(options);
+		 driver = new ChromeDriver(options);
 		  //driver= new FirefoxDriver();
 		  //driver= new EdgeDriver();
 		  driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
